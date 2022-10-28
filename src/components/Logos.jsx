a103e424412f2ad8608dad36f9c1bca70886338f
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-10-28 09:06:38 +0100

    feat: Design logo section

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import slack from "../assets/slack.png";
+import github from "../assets/github.png";
 
 const Logos = () => {
-  return <div>Logos</div>;
+  return (
+    <div className="logos">
+      <img src={slack} alt="slack" />
+      <img src={github} alt="github" />
+    </div>
+  );
 };
 
 export default Logos;
