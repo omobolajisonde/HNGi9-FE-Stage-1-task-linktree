b798a15c1cf557cff7d5b09218a56410ad39655a
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-10-28 05:08:17 +0100

    feat: Design profile section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+import { Profile, Logos, Link, Footer } from "./components";
+
+function App() {
+  return (
+    <>
+      <Profile />
+      <Link />
+      <Logos />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import pic from "../assets/pic.jpg";
+
+const Profile = () => {
+  const [showSlack] = useState(false);
+  return (
+    <header>
+      <img src={pic} alt="profile img" id="profile__img" />
+      <p id="twitter">iamsonde</p>
+      {showSlack && <p id="slack">jhay.js</p>}
+    </header>
+  );
+};
+
+export default Profile;
